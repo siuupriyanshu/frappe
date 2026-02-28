46c2401c28d6a85c29aa13095fa6d4f912b1aee8
siuupriyanshu <mandalpriyanshu22@gmail.com>
2026-02-28 23:32:23 +0545

    appointment-booking

---
+++ appointment_system/appointment_system/doctype/doctor/doctor.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, priyanshu mandal and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Doctor(Document):
+	pass
