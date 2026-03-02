aced18808ad1e48240272a8f6ea0250d3e43e238
siuupriyanshu <mandalpriyanshu22@gmail.com>
2026-03-02 17:22:01 +0545

    create appointment doctype

--- appointment_system/appointment_system/doctype/doctor/doctor.py
+++ appointment_system/appointment_system/doctype/doctor/doctor.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2026, priyanshu mandal and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Doctor(Document):
-	pass
+    def validate(self):
+        self.validate_times()
+        self.validate_email()
+
+    def validate_times(self):
+        if self.start_time and self.end_time:
+            if self.start_time >= self.end_time:
+                frappe.throw('Start Time must be before End Time')
+
+    def validate_email(self):
+        existing = frappe.db.exists('Doctor', {'email': self.email})
+        if existing and existing != self.name:
+            frappe.throw(f'Email {self.email} is already registered to another doctor')
+
+    def after_insert(self):
+        frappe.msgprint(f'Doctor {self.full_name} created successfully', alert=True)
+
