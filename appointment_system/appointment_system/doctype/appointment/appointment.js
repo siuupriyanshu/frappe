fe53a3e1e40061cafaf52b4a34b879811d5e98e4
siuupriyanshu <mandalpriyanshu22@gmail.com>
2026-03-02 19:10:23 +0545

    client scripts

--- appointment_system/appointment_system/doctype/appointment/appointment.js
+++ appointment_system/appointment_system/doctype/appointment/appointment.js
@@ -1,8 +1,64 @@
-// Copyright (c) 2026, priyanshu mandal and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Appointment', {
 
-// frappe.ui.form.on("Appointment", {
-// 	refresh(frm) {
+    // Triggered when the form first loads
+    setup: function(frm) {
+        frm.set_query('doctor', function() {
+            return { filters: { is_active: 1 } };
+        });
+    },
 
-// 	},
-// });
+    // Auto-populate fields when doctor is selected
+    doctor: function(frm) {
+        if (frm.doc.doctor) {
+            frappe.call({
+                method: 'frappe.client.get',
+                args: { doctype: 'Doctor', name: frm.doc.doctor },
+                callback: function(r) {
+                    if (r.message) {
+                        frm.set_value('doctor_name', r.message.full_name);
+                        frm.set_value('consultation_fee', r.message.consultation_fee);
+                        frm.set_value('duration', r.message.slot_duration);
+                    }
+                }
+            });
+        }
+    },
+
+    // Auto-set combined datetime when date changes
+    appointment_date: function(frm) {
+        set_appointment_datetime(frm);
+        validate_appointment_date(frm);
+    },
+
+    // Auto-set combined datetime when time changes
+    appointment_time: function(frm) {
+        set_appointment_datetime(frm);
+    },
+
+    // Validate before form save
+    validate: function(frm) {
+        validate_appointment_date(frm);
+    }
+});
+
+function set_appointment_datetime(frm) {
+    if (frm.doc.appointment_date && frm.doc.appointment_time) {
+        let dt = frm.doc.appointment_date + ' ' + frm.doc.appointment_time;
+        frm.set_value('appointment_datetime', dt);
+    }
+}
+
+function validate_appointment_date(frm) {
+    if (frm.doc.appointment_date) {
+        let today = frappe.datetime.get_today();
+        if (frm.doc.appointment_date < today) {
+            frappe.msgprint({
+                title: 'Invalid Date',
+                message: 'Appointment date cannot be in the past.',
+                indicator: 'red'
+            });
+            frm.set_value('appointment_date', '');
+        }
+    }
+}
+
--- appointment_system/appointment_system/doctype/doctor/doctor.js
+++ appointment_system/appointment_system/doctype/doctor/doctor.js
@@ -1,8 +1,22 @@
-// Copyright (c) 2026, priyanshu mandal and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Doctor', {
+    refresh: function(frm) {
+        if (!frm.is_new()) {
+            frm.add_custom_button('View Appointments', function() {
+                frappe.set_route('List', 'Appointment', {
+                    doctor: frm.doc.name
+                });
+            }, 'Go To');
+        }
+    },
+    start_time: function(frm) { validate_times(frm); },
+    end_time: function(frm) { validate_times(frm); }
+});
 
-// frappe.ui.form.on("Doctor", {
-// 	refresh(frm) {
-
-// 	},
-// });
+function validate_times(frm) {
+    if (frm.doc.start_time && frm.doc.end_time) {
+        if (frm.doc.start_time >= frm.doc.end_time) {
+            frappe.msgprint('Start time must be before end time.');
+            frm.set_value('end_time', '');
+        }
+    }
+}
